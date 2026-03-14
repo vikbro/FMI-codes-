e61626711c1bbf11afeb0d2ce9f9c4ff13c7dbe9
kokomosquito <detistovn@gmail.com>
2026-03-14 12:42:20 +0200

    working launch

---
+++ vector.gd
@@ -0,0 +1,45 @@
+extends Node2D
+
+var vector : Vector2
+var R
+var L
+var r = 10
+var launch_traj
+var direction: Vector2
+var orbit_vector: Vector2
+var vx
+var vy
+
+func _ready() -> void:
+	$Label.visible = false
+	$LineEdit.visible = false
+
+
+func _draw() -> void:
+	draw_line(Vector2.ZERO, vector, Color.BLACK, 5)
+	draw_line(direction, orbit_vector, Color.RED, 7)
+
+func _process(delta: float) -> void:
+	var theta = - await Event.angle_input
+	if theta >= 0 || theta < -180:
+		pass
+	else:
+		print(-theta)
+		var x = 10 * cos(deg_to_rad(theta))
+		var y = 10 * sin(deg_to_rad(theta))
+		R = abs(x/cos(deg_to_rad(theta)))
+		direction = abs(y/2) * Vector2(x, y)
+		L = abs(theta) + rad_to_deg(atan((sqrt(x * x + y * y) * cos(deg_to_rad(theta))) / r))
+		vx =  (R * cos(deg_to_rad(L)) * cos(deg_to_rad((L - theta))) + abs(y/2) * x)
+		vy = - (R * sin(deg_to_rad((90 - L))) * cos(deg_to_rad((L - theta))) + abs(y/2) * y)
+		vector = Vector2(x, y)
+		orbit_vector = Vector2(vx,  vy)
+		var speed = Vector2(0, orbit_vector.y)
+		var height = Vector2(orbit_vector.x, 0)
+		queue_redraw()
+
+
+func _on_button_pressed() -> void:
+	$Label.visible = true
+	$LineEdit.visible = true
+	
