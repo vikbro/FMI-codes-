0f98c319f88c9f8dfece071794c837c2f0b39500
kokomosquito <detistovn@gmail.com>
2026-03-15 09:07:08 +0200

    sound

--- enemy.gd
+++ enemy.gd
@@ -5,15 +5,22 @@ extends Node2D
 #@onready var path_follow_2d: PathFollow2D = $".."
 var track: bool = false
 var area_signal: Area2D
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
+
 
 func _ready() -> void:
+	#$AudioStreamPlayer.play()
 	pass
 
 func _process(delta: float) -> void:
-	pass
+	_take_damage(12)
 
-func _take_damage(dmg):
+func _take_damage(dmg: int):
+	await get_tree().create_timer(2.0).timeout
 	stats.health = stats.health - dmg
+	$AudioStreamPlayer.play()
+	await $AudioStreamPlayer.finished
+
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.get_script() == get_script():
